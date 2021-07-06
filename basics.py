510ebb933d1aaa2b192ff52ce67761a40c26bf76
kaziNAHIAN <provat44.knhp@gmail.com>
2021-07-06 22:32:39 +0600

    The beginning

---
+++ basics.py
@@ -0,0 +1,23 @@
+age = 20 
+
+if age < 18:
+	print("You Can watch Other movies")
+elif age >= 18 and age < 21:
+	print("You can not watch this movie enjoy other movies !!")
+else:
+	print("Enjoy ... ")
+
+
+name = 'Nahian'
+
+if name == 'Mahi':
+	print("welcome, ",name)
+elif name == 'Nahian':
+	print('Hello, ',name)
+else:
+	print("I don't know you")
+
+fruits = ['apples','oranges','mangoes']
+
+for f in name:
+	print(f)
